70d3abc46b1ea8c92203f2469d57c0faba94ce29
bhartmk257 <bharatmk257@gmail.com>
2018-09-16 02:08:39 +0530

    code done not output so trying to mess up code

--- app/src/main/java/com/example/bharat/news/MainActivity.java
+++ app/src/main/java/com/example/bharat/news/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.bharat.news;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+    implements LoaderManager.LoaderCallbacks<List<News>> {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    private static final String THEGUARDIAN_NEWS_REQUEST_URL = "https://content.guardianapis.com/search?api-key=f74bf62e-74fd-4b75-b6ba-3670d1188b36";
+
+    private static final int NEWS_LOADER_ID = 1;
+
+    private NewsAdapter mAdapter;
+
+    private TextView mEmptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView newsListView = (ListView) findViewById(R.id.news_list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_text_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        newsListView.setAdapter(mAdapter);
+
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                News currentNews = mAdapter.getItem(position);
+                Uri newsUri = Uri.parse(currentNews.getmUrl());
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+                startActivity(websiteIntent);
+            }
+        });
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager LoaderManager = getLoaderManager();
+            LoaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+
+            View loadingIndicator = findViewById(R.id.news_progressbar);
+            loadingIndicator.setVisibility(View.GONE);
+            mEmptyStateTextView.setText(R.string.no_internet);
+        }
+    }
+
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+
+        return new NewsLoader(this,THEGUARDIAN_NEWS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
+
+        mAdapter.clear();
+        if (data !=null && !data.isEmpty()) {
+            mAdapter.addAll(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
+
     }
 }
